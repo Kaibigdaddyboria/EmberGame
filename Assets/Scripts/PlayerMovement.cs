84faa11ecafb19087784ae1bae4e3b32556838f0
Kaibigdaddyboria <132318880+Kaibigdaddyboria@users.noreply.github.com>
2024-06-14 17:17:58 +1000

    Wall Jump
    I worked on the code for the wall jumping and sliding

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,19 +16,37 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private TrailRenderer tr;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
     private bool CanDash = true;
     private bool isDashing;
     private float dashingPower = 24f;
     private float dashingTime = 0.2f;
     private float dashingCooldown = 1f;
+    private bool isWallSliding;
+    private float wallSlidingSpeed;
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0f;
+    private Vector2 wallJumpingPower = new Vector2(16f, 16f);
 
     // Start is called before the first frame update
     void Update()
     {
-        Flip();
+        
         if (isDashing)
         {
             return;
+        }
+        WallSlide();
+        WallJump();
+        
+        if (!isWallJumping)
+        {
+            Flip();
         }
         float yRotation = transform.eulerAngles.y;
         if (Input.GetKeyDown(KeyCode.Escape) && CanDash && (Mathf.Abs(yRotation - 0) < 1))
@@ -54,10 +72,6 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
-        //else if (Input.GetKeyDown("c"))
-        //{
-        //    m_animator.SetTrigger("Attack");
-        //}
         else if (Mathf.Abs(horizontal) > Mathf.Epsilon && grounded)
             m_animator.SetInteger("AnimState", 2);
         else
@@ -72,10 +86,11 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        Vector2 velocity = rb.velocity;
+        if (!isWallJumping)
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        }
     }
-
     private bool IsGrounded()
     {
         bool grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
@@ -107,5 +122,58 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(dashingCooldown);
         CanDash = true;      
     }
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
 
+    private void WallSlide()
+    {
+        if(IsWalled() && !IsGrounded() && horizontal !=0f)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallSliding = false;
+            wallJumpingDirection = horizontally;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (horizontally != wallJumpingDirection)
+            {
+                isFacingRight = !isFacingRight;
+                transform.Rotate(0f, 180f, 0f);
+                horizontal = -1;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
 }
